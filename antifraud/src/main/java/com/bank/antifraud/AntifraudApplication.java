21978c89015f7920673bc5408f33b07178b977d3
Anastasiia0505 <prozorovaa47@gmail.com>
2024-08-09 16:35:04 +0300

    Устранение замечаний после проверки. Переделаны импорты, удалены комментарии, лишние пробелы в коде

--- antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
+++ antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
 @SpringBootApplication
 @EnableEurekaClient
 public class AntifraudApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(AntifraudApplication.class, args);
-
     }
 }
--- antifraud/src/main/java/com/bank/antifraud/aspect/AuditAspect.java
+++ antifraud/src/main/java/com/bank/antifraud/aspect/AuditAspect.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
 
 @Aspect
